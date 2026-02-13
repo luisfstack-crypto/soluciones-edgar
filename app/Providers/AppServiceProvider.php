aef0de39729a172545c989d17f3b14d119a1ea13
GrimmFuzz <luisfstack@gmail.com>
2026-02-13 12:43:24 -0600

    fix: usar trustProxies para manejo correcto de HTTPS en railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -28,10 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
-
         \Illuminate\Database\Eloquent\Model::shouldBeStrict();
         \App\Models\Order::observe(\App\Observers\OrderObserver::class);
 
