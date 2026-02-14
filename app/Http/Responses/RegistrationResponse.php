c38de21228d8d02d69d42285cae57c0411ddfce2
GrimmFuzz <luisfstack@gmail.com>
2026-02-14 11:40:09 -0600

    fix: RegistrationResponse for liveware

--- app/Http/Responses/RegistrationResponse.php
+++ app/Http/Responses/RegistrationResponse.php
@@ -4,10 +4,11 @@ namespace App\Http\Responses;
 
 use Filament\Http\Responses\Auth\Contracts\RegistrationResponse as RegistrationResponseContract;
 use Illuminate\Http\RedirectResponse;
+use Livewire\Features\SupportRedirects\Redirector;
 
 class RegistrationResponse implements RegistrationResponseContract
 {
-    public function toResponse($request): RedirectResponse
+    public function toResponse($request): RedirectResponse|Redirector
     {
         return redirect()->to('/app');
     }
